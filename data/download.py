0b4fb800edc99a0ba0699d3599f9721abe42c433
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 15:50:57 +0000

    Adding numeric_owner as keyword arguement

--- data/download.py
+++ data/download.py
@@ -45,7 +45,7 @@ def ungzip(filepath):
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(zf, dirpath)
